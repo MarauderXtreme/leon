96cf13b19ccb370ce19a3ea0d6f4ac9ff4cdc967
Christoph Kepler <development@kepler.international>
2016-10-18 12:28:39 +0200

    Little easteregg

--- js/app.js
+++ js/app.js
@@ -1,7 +1,23 @@
+function easteregg() {
+	console.log(`%c This site was build by`, "color:#00050f; font-family:monospace");
+	console.log(`%c
+███╗   ███╗ █████╗ ██████╗  █████╗ ██╗   ██╗██████╗ ███████╗██████╗ ██╗  ██╗████████╗██████╗ ███████╗███╗   ███╗███████╗
+████╗ ████║██╔══██╗██╔══██╗██╔══██╗██║   ██║██╔══██╗██╔════╝██╔══██╗╚██╗██╔╝╚══██╔══╝██╔══██╗██╔════╝████╗ ████║██╔════╝
+██╔████╔██║███████║██████╔╝███████║██║   ██║██║  ██║█████╗  ██████╔╝ ╚███╔╝    ██║   ██████╔╝█████╗  ██╔████╔██║█████╗  
+██║╚██╔╝██║██╔══██║██╔══██╗██╔══██║██║   ██║██║  ██║██╔══╝  ██╔══██╗ ██╔██╗    ██║   ██╔══██╗██╔══╝  ██║╚██╔╝██║██╔══╝  
+██║ ╚═╝ ██║██║  ██║██║  ██║██║  ██║╚██████╔╝██████╔╝███████╗██║  ██║██╔╝ ██╗   ██║   ██║  ██║███████╗██║ ╚═╝ ██║███████╗
+╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝╚══════╝
+`, "color:#ff6600; font-size:10px; font-family:monospace");
+	console.log(`%c I like your enthusiasm but here you won't find any content here`, "color:#00050f; font-family:monospace");
+	console.log(`%c If you are interested in the source-code go to github.com/marauderxtreme/leon`, "color:#00050f; font-family:monospace");
+}
+
 $(document).ready(function(){
+
 	// Foundation JavaScript
 	// Documentation can be found at: https://foundation.zurb.com/sites/docs/
 	$(document).foundation();
+
 	// Slick Slider Configuration
 	// Documentation can be found at: https://kenwheeler.github.io/slick/
 	$('.index-slider').slick({
@@ -14,4 +30,5 @@ $(document).ready(function(){
 		// Disable navigation arrows
 		arrows: false
 	});
+	easteregg();
 });
